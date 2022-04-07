6c06ef3e6037ccf1de17fec989b2f11f4ae78c71
raz-mon <razmon@post.bgu.ac.il>
2022-04-07 11:37:02 +0300

    req - add numpy

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 # token-gen https://www.operate-first.cloud/apps/content/observatorium/thanos/thanos_programmatic_access.html
-# OPERATE_FIRST_TOKEN = "sha256~QQg_ZsK2BoLtyH4yCvbjbWKqvCksCr1v3UQadK5iXdM"
 OPERATE_FIRST_TOKEN = "sha256~QdJLfZTaZ_HYuvgt6OWv2bNoF70fy1iFG-jbbqNXWrk"
 THANOS_URL = "https://thanos-query-frontend-opf-observatorium.apps.smaug.na.operate-first.cloud"
